b0044f654bfc6aaf811530cb144604861a741836
Onno de Vries <97830189+Odvries@users.noreply.github.com>
2025-01-03 00:10:28 +0100

    Added basic math functions

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 const dContainer = document.querySelector("#maincontainer");
 const BUTTONS = ["0","",
                  "AC","()","%","/",
@@ -11,6 +6,12 @@ const BUTTONS = ["0","",
                  "1","2","3","+",
                  "0",",","<-","="];
 
+
+
+let operand1 = 0;
+let operator = "";
+let operand2 = 0;
+     
 function addDiv(parentElement,divXSize,divYSize,buttonOrder) {
     const newDiv = document.createElement("div");
     newDiv.setAttribute("id", "button"+buttonOrder);
@@ -26,7 +27,7 @@ function addDiv(parentElement,divXSize,divYSize,buttonOrder) {
             newDiv.style.backgroundColor = 'rgb(47, 47, 48)'
         });
         newDiv.addEventListener("click", function(){
-            operate(buttonOrder);
+            perform(buttonOrder);
         });
     }
 
@@ -50,29 +51,76 @@ function createGrid() {
     }
 }
 
-function operate(action) {
+
+function clearVariables() {
+    operand1 = 0;
+    operator = "";
+    operand2 = 0;
+}
+
+function perform(action) {
     const dScreen = document.querySelector("#text0");
     switch (action) {
         case 2:
             dScreen.text = BUTTONS[0];
+            clearVariables();
             break;
         case 3:
+            if (operator === "") 
+                operator = "("
+            else if (operator === "(") operator = ")";
+            break;
         case 4:
         case 5:
         case 9:
         case 13:
         case 17:
+            if (operator === "") {
+                operator = BUTTONS[action];
+                operand1=Number(dScreen.text);
+                dScreen.text = dScreen.text + operator;
+            }
+            break;            
         case 19:
         case 20:
-        case 21:
             console.log(action);
+            break;
+        case 21:     
+            if (operator !="") {
+                operand2 = dScreen.text.substring(dScreen.text.indexOf(operator) + 1);
+                if (operand2 != "") {
+                    operand2 = Number(operand2);
+                    operate();
+                }
+            }    
             break;
         default:
             dScreen.text = dScreen.text===BUTTONS[0]? BUTTONS[action]:(dScreen.text + BUTTONS[action]);
     }
-
 }
 
+function operate() {
+    const dScreen = document.querySelector("#text0");
+    let calcResult = 0;
+    console.log(operand1 + operator + operand2);
+    switch (operator) {
+        case "+":
+            calcResult = operand1 + operand2;
+            break;
+        case "-":
+            calcResult = operand1 - operand2;
+            break;
+        case "X":
+            calcResult = operand1 * operand2;
+            break;                
+        case "/":
+            calcResult = operand1 / operand2;
+            break;
+        }
+    dScreen.text = calcResult;
+    clearVariables();
+    operand1 = calcResult;
+}
 
 createGrid();
 
